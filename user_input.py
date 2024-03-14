4c162982942dfbe23c12c8364c0d259027a181aa
Suleiman Wainaina <wainainasolomon12@gmail.com>
2024-03-14 14:28:03 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,8 @@
+name = input("What is your name?: ")
+age = int(input("What is  your age?:"))
+Location = input("Where are you located?:")
+
+
+print("Hello  "+ name)
+print("You are "+str(age)+" years old")
+print("You live in "+ Location)
